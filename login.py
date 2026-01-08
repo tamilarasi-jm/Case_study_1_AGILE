91d46b93cf29db525baf9288c6a1b7afc80c1deb
taara <tamilarasi022006@gmail.com>
2026-01-08 21:42:12 +0530

    push the remote content

--- login.py
+++ login.py
@@ -18,3 +18,16 @@ library = Library()
 library.add_book("self health and well being")
 library.add_book("Data Structures")
 library.view_books()
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book_name):
+        self.books.append(book_name)
+        print(f'Book "{book_name}" added to the library.')
+
+    def view_books(self):
+        if not self.books:
+            print("No books in the library.")
+        else:
+            print("Books in the library:")
